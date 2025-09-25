3522bed1cbfea0f6892d90462c39c360b0c66cc3
Dhanvin Pratap Singh <dhanvinprsingh@gmail.com>
2025-09-25 11:42:58 +0530

    Add files via upload

---
+++ days in a month.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("enter a number b/w 1-12: \n");
+    scanf("%d", &n);
+
+    switch(n)
+    {
+        case 1: case 3 : case 5 : case 7 : case 8: case 10: case 12: printf("31 days");
+        break;
+        case 2: printf("28 days");
+        break;
+        case 4: case 6: case 9: case 11: printf("30 days");
+        break;
+
+    }
+}
